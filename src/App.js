725d7e28074b2cd6aa58d111bc05eb87729a3236
Jayson Lewis <jayson.lewis5@gmail.com>
2022-04-04 23:18:55 -0500

    Main app completed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
 
-import {React, useEffect, useState} from 'react';
+import {React, useState} from 'react';
 
 import './App.css';
 
-const ModalButton = ({z, modals, setModals}) => {
-  const randTop = Math.floor(Math.random() * (window.innerHeight-200));
-  const randLeft = Math.floor(Math.random() * (window.innerWidth-300));
+const ModalButton = ({modals, setModals, setIsLoading}) => {
+  const randTop = Math.floor(Math.random() * (window.innerHeight-400));
+  const randLeft = Math.floor(Math.random() * (window.innerWidth-400));
 
-  function handleClick (e) {
+  async function handleClick (e) {
     e.preventDefault();
+    const delay = ms => new Promise(res => setTimeout(res, ms));
     
+    setIsLoading(true);
+    await delay(2000);
+
+    if(modals.length > 10){
+      modals.shift();
+    }
+
     setModals([...modals,  {
-      id:modals.length+1,
-      index: z+10,
+      id:Math.floor(Math.random() * 1000),
       rTop: randTop,
       rLeft: randLeft
     }])
-
+    setIsLoading(false);
     console.log(modals)
   }
 
   return(
     <>
-      <button className='ModalButton'  onClick={handleClick}>Modal</button>
+      <button className='modal-button'  onClick={handleClick}>Make a modal</button>
     </>
   )
 
 }
 
-const Modal = ({id, index, z, modals, setModals, rTop, rLeft}) =>{
+const Modal = ({id, setIsLoading, modals, setModals, rTop, rLeft}) =>{
   
   return (
 
-    <div className='modal-wrapper' style={{
-      zIndex: index
-    }}>
-      <div className='Modal' style={{
-        zIndex: index+50,
-        top: rTop,
-        left: rLeft,     
-      }}
+    <div className='modal-wrapper'>
+      <div className='Modal' style={{top: rTop,left: rLeft}}
       >
-        <button className='close-modal' onClick={() => setModals(modals.filter(item => item.id !== id))}>X</button>
-        <h1 className='ModalTitle'>Modal!</h1>
-        <p>This is a new modal window. You did it!</p>
-        <ModalButton z={z} setModals={setModals} modals={modals} />
+        
+        <h1 className='ModalTitle'>
+          <img src="djkahlid.png" style={{width: "300px"}}/>
+          Anotha one!</h1>
+        <p>Click Create a Modal to make another modal!</p>
+        <button className='close-modal' onClick={() => setModals(modals.filter(item => item.id !== id))}>Close modal</button>
+        <ModalButton setIsLoading={setIsLoading}  setModals={setModals} modals={modals} />
+        
       </div>
     </div>    
   )
@@ -52,19 +57,21 @@ const Modal = ({id, index, z, modals, setModals, rTop, rLeft}) =>{
 
 function App() {
 
-  const [z, setZ] = useState(10);
-  const [modals, setModals] = useState([])
+  const [modals, setModals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   return(
   <div className='container'>
-    <h1 className='main-title'>Modal Bonanza</h1>
-    <p>Click the button to start making infinite modals!</p>
-    <ModalButton z={z} setModals={setModals} modals={modals}  />
+    {isLoading ? <div className="dataLoading">
+      <div className="spinner"></div>DATA LOADING</div> : ''}
+    <h1 className='main-title'>Modal Generator</h1>
+    <p>Click the button below to create modals on modals!</p>
+    <ModalButton setIsLoading={setIsLoading} setModals={setModals} modals={modals}  />
       {(modals.length > 0) ?  
         modals.map(modal => {
           return(
           <div key={modal.id} >
-          <Modal z={z} modals={modals} setModals={setModals} rTop={modal.rTop} rLeft={modal.rLeft} id={modal.id} index={modal.index} />
+          <Modal  setIsLoading={setIsLoading} modals={modals} setModals={setModals} rTop={modal.rTop} rLeft={modal.rLeft} id={modal.id} index={modal.index} />
           </div>)
         })
       : ''}
